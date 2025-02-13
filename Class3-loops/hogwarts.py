1a92310ef6944e0deaa20908cde74ffcd55a5f1e
debs-sa <debs.saas@gmail.com>
2025-02-13 11:35:53 -0700

    class 3 uploads

--- Class3-loops/hogwarts.py
+++ Class3-loops/hogwarts.py
@@ -14,4 +14,45 @@ for student in students:
 students = ["Hermione", "Harry", "Ron"]
 
 for i in range(len(students)):
-    print(i + 1, students[i]) 
+    print(i + 1, students[i])
+
+
+
+
+## dict ## dictionary
+ 
+students = ["Hermione", "Harry", "Ron", "Draco"]
+houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
+
+students = {
+    "Hermione": "Gryffindor", 
+    "Harry": "Gryffindor", 
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+}
+
+print(students)
+
+print(students["Harry"])
+
+for student in students:
+     print(student, students[student], sep=", ") # python feature for loops
+     
+     
+     #
+############# DICTIONARY #################
+# uses {}
+# word and definition
+#a list just uses []
+
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russel Terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None},    #absense of a value
+]
+
+for student in students:
+    print(student["name"])
+    print(student["name"], student["house"], student["patronus"], sep=", ")
+
--- Class3-loops/test.py
+++ Class3-loops/test.py
@@ -1,16 +1,11 @@
-def main():
-    number = get_number()
-    meow(number)
-    
-def  get_number():
-    while True:
-        n = int(input("What's n? "))
-        if n > 0:
-            break
-    return n
-    
-def meow(n):
-    for _ in range(n):
-        print("meow")
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russel Terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None},    #absense of a value
+]
 
-main()
+for student in students:
+    print(student["name"])
+    print(student["name"], student["house"], student["patronus"], sep=", ")
+    
